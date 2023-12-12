da39c2e71712e80de77c83ea69868793421c7aee
Mohammad Aseem <aseemmohammad73@gmail.com>
2023-12-12 12:53:10 +0530

    Footer change

--- src/Footer1/Footer1.jsx
+++ src/Footer1/Footer1.jsx
@@ -15,21 +15,28 @@ function Footer1() {
           <li>
             <h4 >Keep connected</h4>
             <div style={{ display: "flex", gap: "30px",padding:"0.3rem 0.7rem",paddingTop:"1rem" }}>
-              
-              <FaInstagramSquare  style={{fontSize:"20px"}}/>
-              <span>Instagram</span>
+              <a href="https://www.instagram.com/ecficio.vnrvjiet/?hl=en" style={{display:"flex",justifyContent:"center",alignContent:"center",flexDirection:"row"}}>
+              <FaInstagramSquare  style={{fontSize:"20px",marginRight:"1vw"}}/>
+              <span>Ecficio_Instagram</span>
+              </a>
             </div>
             <div style={{ display: "flex", gap: "30px",padding:"0.3rem 0.7rem" }}>
-              <BsTwitterX style={{fontSize:"20px"}}/>
-              <span>X</span>
+              <a href="https://twitter.com/EcficioVNRVJIET"  style={{display:"flex",justifyContent:"center",alignContent:"center",flexDirection:"row"}}>
+              <BsTwitterX style={{fontSize:"20px",marginRight:"1vw"}}/>
+              <span>Ecficio_X</span>
+              </a>
             </div>
             <div style={{ display: "flex", gap: "30px" ,padding:"0.3rem 0.7rem"}}>
-              <FaFacebookF style={{fontSize:"20px"}} />
-              <span>Facebook</span>
+              <a href="https://www.facebook.com/ecficio.vnrvjiet/"  style={{display:"flex",justifyContent:"center",alignContent:"center",flexDirection:"row"}}>
+              <FaFacebookF style={{fontSize:"20px",marginRight:'1vw'}} />
+              <span>Ecficio_Facebook</span>
+              </a>
             </div>
             <div style={{ display: "flex", gap: "30px",padding:"0.3rem 0.7rem" }}>
-              <FaLinkedin style={{fontSize:"20px"}} />
-              <span>Linkedin</span>
+              <a href="https://www.linkedin.com/in/ecficiovnrvjiet/?originalSubdomain=in" style={{display:"flex",justifyContent:"center",alignContent:"center",flexDirection:"row"}}>
+              <FaLinkedin style={{fontSize:"20px",marginRight:"1vw"}} />
+              <span>Ecficio_linkedIn</span>
+              </a>
             </div>
             
           </li>
@@ -37,11 +44,11 @@ function Footer1() {
             <h4>Contact Us On</h4>
             <div style={{ display: "flex", gap: "30px",padding:"0.3em 0.7em",paddingTop:"1em" }}>
               <BiPhoneCall style={{fontSize:"20px"}} />
-              <span>phone number 1</span>
+              <span>Rohan Poluri(+91-62813 86790)</span>
             </div>
             <div style={{ display: "flex", gap: "30px",padding:"0.3em 0.7em",paddingBottom:"1.5em" }}>
               <BiPhoneCall style={{fontSize:"20px"}} />
-              <span>Phone number 2</span>
+              <span>Anup Srivastav (+91-70931 74138)</span>
             </div>
             
 
@@ -50,7 +57,9 @@ function Footer1() {
           <li>
              <h4 style={{paddingBottom:"0.5em"}}>FOR ANY QUERIES</h4>
              <div style={{display:"flex",flexDirection:"column"}}>
+              <a href="mailto:ecficio@vnrvjiet.in">
             <span style={{padding:"0.3em 0.7em"}}>ecficio@vnrvjiet.in</span>
+            </a>
            </div>
           </li>
         </ul>
